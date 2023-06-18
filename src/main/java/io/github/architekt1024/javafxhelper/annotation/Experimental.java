4f79a298f22b5477059c0b78240f38cf3f2c8089
architekt1024 <architekt1024@users.noreply.github.com>
2023-06-19 00:21:09 +0200

    update license header

--- src/main/java/io/github/architekt1024/javafxhelper/annotation/Experimental.java
+++ src/main/java/io/github/architekt1024/javafxhelper/annotation/Experimental.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 architekt1024
+ * Copyright 2020-2023 architekt1024
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
